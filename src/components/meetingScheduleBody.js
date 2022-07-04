1a37a40a53f9e9452c2794d5db64971f0b08648c
myGithub123# <royandugu99009@gmail.com>
2022-07-04 19:21:57 +0545

    Edited meeting container

--- src/components/meetingScheduleBody.js
+++ src/components/meetingScheduleBody.js
@@ -1,18 +1,37 @@
+import {useState} from "react";
 import "./meetingScheduleBody.css";
 
-const meetingScheduleBody=({data,status})=>{
+const MeetingScheduleBody=({data,status})=>{
+    const [containerHeight,setContainerHeight]=useState(window.innerHeight);
+    window.onresize=()=>setContainerHeight(window.innerHeight);
+    let containerCls;
+
+    const assignCls=(cls)=>{
+        containerCls=cls;
+    }
+
+
     if(status==="loading") return (<h1> Loading... </h1>);
     if(status==="error") return (<h1> Error... </h1>);
     return(
-        <div className="meetingContainer">
+        <div className="meetingContainer" style={{height:containerHeight-165}}>
         {
             data.map(index=>(
-                <div className="invMeetingContainer" key={index.id}>
-                    <h5> {index.name} </h5> 
-                </div>
+                    <div key={index.id}>
+                        {(index.semMenu==="Important")?assignCls("invMeetingContainer dangerContainer"):
+                        (index.semMenu==="Link")?assignCls("invMeetingContainer primaryContainer"):
+                        (index.semMenu==="Note")?assignCls("invMeetingContainer warningContainer"):
+                        assignCls("invMeetingContainer darkContainer")}
+                        <div className={containerCls} key={index.mid}>
+                            <h5> {index.name} </h5> 
+                            <p> {index.time}</p>
+                            <button className="btn btn-dark"> Get Details </button> 
+                            <button className="btn btn-light"> Join meeting </button>  
+                        </div>
+                    </div>
             ))
         }
         </div>
     );
 }
-export default meetingScheduleBody;
+export default MeetingScheduleBody;
