ac18004669096f8c17fa30cd284041d6ef0ce95e
myGithub123# <royandugu99009@gmail.com>
2022-07-19 16:55:24 +0545

    Identing and re-checking

--- src/App.js
+++ src/App.js
@@ -20,11 +20,13 @@ const App=()=>{
     return(
         <div className="container-fluid">
             <div className="row">
+
                 <div className="col-lg-2 col-12 menuComp d-none d-lg-block">
                     <div className="posFixed">
                         <SideMenu/>
                     </div>
                 </div>
+
                 <div className="col-12 d-block d-lg-none" style={{zIndex:2}}>
                     <div className="posFixedSml bg-dark">
                         <div className="flexBox">
@@ -32,30 +34,34 @@ const App=()=>{
                         </div>
                     </div>
                 </div>
+
                 <div className="col-lg-5 taskListContainer d-none d-lg-block">
                     <BrowserRouter>
                         <TaskLister fetcher={fetchTasks} containerHeight={containerHeight}/>
                     </BrowserRouter>
                 </div>
+
                 <div className="col-lg-5 d-none d-lg-block">
                     <BrowserRouter>
                         <MeetingSchedulesHeader fetcher={fetchMeetings} containerHeight={containerHeight}/>
                     </BrowserRouter>
                 </div>
             </div>
+
+
             <div className="d-block d-lg-none">
                 <BrowserRouter className="flexMenuDisplay">
-                    <TaskLister fetcher={fetchTasks} containerHeight={containerHeight}/>
-                    <MeetingSchedulesHeader fetcher={fetchMeetings} containerHeight={containerHeight}/>
                 </BrowserRouter>
             </div>
-           <div className="bottomBarNavigation bg-dark d-block d-lg-none">
+
+            <div className="bottomBarNavigation bg-dark d-block d-lg-none">
                 <div className="bottomButtonContainer">
                     <BrowserRouter>
                         <BottomBarNavigation/>
                     </BrowserRouter>
                 </div>
             </div>
+            
         </div>
     )
 } 
