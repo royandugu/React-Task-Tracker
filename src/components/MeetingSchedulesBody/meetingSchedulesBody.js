15ddd72f5ac6439b076d6464d332acfa10eeb718
myGithub123# <royandugu99009@gmail.com>
2022-07-24 13:45:47 +0545

    Meeting deleter declaration

--- src/components/MeetingSchedulesBody/meetingSchedulesBody.js
+++ src/components/MeetingSchedulesBody/meetingSchedulesBody.js
@@ -2,7 +2,9 @@ import QueryState from "../QueryState/queryState";
 import "./meetingSchedulesBody.css";
 
 const MeetingScheduleBody=({stateData,queryFetch,setData,status,containerHeight})=>{
-    
+    const deleter=()=>{
+        /* Here will consist the meeting delete function */
+    }
     return(
         <div className="meetingContainer" style={{height:containerHeight-165}}>
             <QueryState stateData={stateData} queryFetch={queryFetch} setData={setData} status={status} deleter={deleter}/>
