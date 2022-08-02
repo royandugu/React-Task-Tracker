56d8b2d3fb416583fab47255da27c78da01d046b
myGithub123# <royandugu99009@gmail.com>
2022-08-02 09:49:32 +0545

    Route prop name conv

--- src/components/Routes/routes.js
+++ src/components/Routes/routes.js
@@ -5,15 +5,15 @@ import Important from "../SubDivision/Important";
 import Links from "../SubDivision/Links";
 import Notes from "../SubDivision/Notes";
 
-const RouteMaker = ({data,deleter,setTask,isTask}) => {
+const RouteMaker = ({stateData,setData,deleter,isTask}) => {
     if(isTask){
         return (
         //Filter out for important
             <Routes>
-                <Route path="/" element={<All tasks={data} setTask={setTask} deleter={deleter}/>}/> 
-                <Route path="/Important" element={<Important tasks={data} setTask={setTask} deleter={deleter}/>} />
-                <Route path="/Notes" element={<Notes tasks={data} setTask={setTask} deleter={deleter}/>} />
-                <Route path="/Links" element={<Links tasks={data} setTask={setTask} deleter={deleter}/>} />
+                <Route path="/" element={<All stateData={stateData} setData={setData} deleter={deleter}/>}/> 
+                <Route path="/Important" element={<Important stateData={stateData} setData={setData} deleter={deleter}/>} />
+                <Route path="/Notes" element={<Notes stateData={stateData} setData={setData} deleter={deleter}/>} />
+                <Route path="/Links" element={<Links stateData={stateData} setData={setData} deleter={deleter}/>} />
             </Routes>
         )
     }
