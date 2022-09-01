4cd7acd78cd0c04fcac969cbf85f793d749dfdcd
myGithub123# <royandugu99009@gmail.com>
2022-09-01 09:19:42 +0545

    Rendering color paletts

--- src/components/MeetingComponents/MeetingRenderer/meetingRenderer.js
+++ src/components/MeetingComponents/MeetingRenderer/meetingRenderer.js
@@ -4,7 +4,7 @@ const MeetingRenderer=({stateData})=>{
     return(
         <>
             {(stateData.length>0)?stateData.map(index=>(
-                <div className="meetingContainerChild meetingDanger" key={index.id}>
+                <div className={`meetingContainerChild ${(index.semMenu==="MostImportant")?"mostImportant":(index.semMenu==="Important")?"important":"classic"}`} key={index.id}>
                     <div className="dateAndTime">
                         <p> {index.date} </p>
                         <p> {index.time} </p>
