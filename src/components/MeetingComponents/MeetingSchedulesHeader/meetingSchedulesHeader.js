3dba2d93f746aaab89a07da20fb65293204662a1
myGithub123# <royandugu99009@gmail.com>
2022-09-01 09:21:05 +0545

    Icon for popup

--- src/components/MeetingComponents/MeetingSchedulesHeader/meetingSchedulesHeader.js
+++ src/components/MeetingComponents/MeetingSchedulesHeader/meetingSchedulesHeader.js
@@ -1,24 +1,34 @@
 import { useState } from "react";
 import {FaUser,FaCheckSquare} from "react-icons/fa";
 import { useQuery } from "react-query";
-import MeetingScheduleBody from "../MeetingSchedulesBody/meetingSchedulesBody";
-import "./meetingSchedulesHeader.css";
+import {AiOutlineMenuUnfold} from "react-icons/ai"; 
 
+import MeetingScheduleBody from "../MeetingSchedulesBody/meetingSchedulesBody";
+import RenderOptionPanel from "../RenderOptions/renderOptionPanel";
+
+import "./meetingSchedulesHeader.css";
 
 
 const MeetingScheduleHeader=({fetcher,containerHeight})=>{
     const {data,status}=useQuery("Key",fetcher);
     const [meetingData,setMeetingData]=useState([]);
+    const [hasClicked,setClicked]=useState(false);
+
     const deleter=()=>{
         //Delete function for meetings
-    }    
+    } 
+
     return(
         <div className="meetingsHeaderContainer"> 
             <div className="iContainer">
                 <FaCheckSquare className="text-primary"/><FaUser/> 
             </div>
             <div className="meetingBodyContainer">
-                <h5 className="containerTitle"> Meeting Schedules </h5>
+                <div className="titleFlex">
+                    <h5 className="containerTitle"> Meeting Schedules </h5>
+                    <AiOutlineMenuUnfold style={{marginLeft:10,cursor:"pointer"}} onClick={()=>(hasClicked)?setClicked(false):setClicked(true)}/>
+                    <RenderOptionPanel hasClicked={hasClicked}/> 
+                </div>
                 <MeetingScheduleBody stateData={meetingData} queryFetch={data} setData={setMeetingData} status={status} containerHeight={containerHeight} deleter={deleter}/>
             </div>
         </div>
